db2fa75dbbc33eb653f7a0899d05e945f2012e1d
ab <abfeder@gmail.com>
2017-07-17 17:06:14 -0700

    credentials: add credentialsSource and an implementation that reads environment variables.
    and switch B2Sample to use it instead of hard-coding one set of credentials.
    this will let us use travis's secure variables to pass credentials to the
    tests.

---
+++ core/src/main/java/com/backblaze/b2/client/credentialsSources/B2CredentialsFromEnvironmentSource.java
@@ -0,0 +1,7 @@
+/*
+ * Copyright 2017, Backblaze, Inc. All rights reserved. 
+ */
+package com.backblaze.b2.client.credentialsSources;
+
+public class B2CredentialsFromEnvironmentSource {
+}
---
+++ core/src/main/java/com/backblaze/b2/client/credentialsSources/B2CredentialsSource.java
@@ -0,0 +1,6 @@
+package com.backblaze.b2.client.credentialsProviders;/*
+ * Copyright 2017, Backblaze, Inc. All rights reserved. 
+ */
+
+public interface B2CredentialsProviders {
+}
