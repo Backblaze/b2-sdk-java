b31e60e340a567eb1b658af773a2404393989354
ab <abfeder@gmail.com>
2017-07-18 17:37:14 -0700

    samples: add upload_file and start adding list_file_versions
    so far, i've only tried small files.

--- samples/src/main/java/com/backblaze/b2/sample/B2.java
+++ samples/src/main/java/com/backblaze/b2/sample/B2.java
@@ -5,14 +5,22 @@
 package com.backblaze.b2.sample;
 
 import com.backblaze.b2.client.B2StorageClient;
+import com.backblaze.b2.client.contentSources.B2ContentSource;
+import com.backblaze.b2.client.contentSources.B2ContentTypes;
+import com.backblaze.b2.client.contentSources.B2FileContentSource;
 import com.backblaze.b2.client.exceptions.B2Exception;
 import com.backblaze.b2.client.structures.B2Bucket;
+import com.backblaze.b2.client.structures.B2FileVersion;
 import com.backblaze.b2.client.structures.B2UpdateBucketRequest;
+import com.backblaze.b2.client.structures.B2UploadFileRequest;
 import com.backblaze.b2.client.webApiHttpClient.B2StorageHttpClientBuilder;
 import com.backblaze.b2.util.B2ExecutorUtils;
 
+import java.io.File;
 import java.io.PrintStream;
 import java.util.Arrays;
+import java.util.Map;
+import java.util.TreeMap;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
@@ -33,32 +41,36 @@ public class B2 implements AutoCloseable {
 
     private static final String USAGE =
             "USAGE:\n" +
-            //"    b2 cancel_all_unfinished_large_files <bucketName>\n" +
-            //"    b2 cancel_large_file <fileId>\n" +
-            "    b2 create_bucket <bucketName> [allPublic | allPrivate]\n" +
-            "    b2 delete_bucket <bucketName>\n" +
-            //"    b2 delete_file_version <fileName> <fileId>\n" +
-            //"    b2 download_file_by_id [--noProgress] <fileId> <localFileName>\n" +
-            //"    b2 download_file_by_name [--noProgress] <bucketName> <fileName> <localFileName>\n" +
-            //"    b2 get_file_info <fileId>\n" +
-            //"    b2 help [commandName]\n" +
-            //"    b2 hide_file <bucketName> <fileName>\n" +
-            "    b2 list_buckets\n" +
-            //"    b2 list_file_names <bucketName> [<startFileName>] [<maxToShow>]\n" +
-            //"    b2 list_file_versions <bucketName> [<startFileName>] [<startFileId>] [<maxToShow>]\n" +
-            //"    b2 list_parts <largeFileId>\n" +
-            //"    b2 list_unfinished_large_files <bucketName>\n" +
-            //"    b2 make_url <fileId>\n" +
-            "    b2 update_bucket <bucketName> [allPublic | allPrivate]\n" +
-            //"    b2 upload_file [--sha1 <sha1sum>] [--contentType <contentType>] [--info <key>=<value>]* \\\n" +
-            //"        [--noProgress] [--threads N] <bucketName> <localFilePath> <b2FileName>\n" +
-            "    b2 version\n";
+                    //"    b2 cancel_all_unfinished_large_files <bucketName>\n" +
+                    //"    b2 cancel_large_file <fileId>\n" +
+                    "    b2 create_bucket <bucketName> [allPublic | allPrivate]\n" +
+                    "    b2 delete_bucket <bucketName>\n" +
+                    //"    b2 delete_file_version <fileName> <fileId>\n" +
+                    //"    b2 download_file_by_id [--noProgress] <fileId> <localFileName>\n" +
+                    //"    b2 download_file_by_name [--noProgress] <bucketName> <fileName> <localFileName>\n" +
+                    //"    b2 get_file_info <fileId>\n" +
+                    //"    b2 help [commandName]\n" +
+                    //"    b2 hide_file <bucketName> <fileName>\n" +
+                    "    b2 list_buckets\n" +
+                    //"    b2 list_file_names <bucketName> [<startFileName>] [<maxToShow>]\n" +
+                    "    b2 list_file_versions <bucketName> [<startFileName>] [<startFileId>] [<maxToShow>]\n" +
+                    //"    b2 list_parts <largeFileId>\n" +
+                    //"    b2 list_unfinished_large_files <bucketName>\n" +
+                    //"    b2 make_url <fileId>\n" +
+                    "    b2 update_bucket <bucketName> [allPublic | allPrivate]\n" +
+                    "    b2 upload_file [--sha1 <sha1sum>] [--contentType <contentType>] [--info <key>=<value>]* \\\n" +
+                    "        [--noProgress] [--threads N] <bucketName> <localFilePath> <b2FileName>\n" +
+                    "    b2 version\n";
 
     // where we should write normal output to.
     private final PrintStream out;
 
     // our client.
     private final B2StorageClient client;
+
+    // overridden by some command line args.
+    private boolean showProgress = true;
+    private int numThreads = 6;
 
     // this is our executor.  use getExecutor() to access it.
     // it's null until the first time getExecutor() is called.
@@ -71,7 +83,7 @@ public class B2 implements AutoCloseable {
 
     private ExecutorService getExecutor() {
         if (executor == null) {
-            executor = Executors.newFixedThreadPool(6, B2ExecutorUtils.createThreadFactory(APP_NAME + "-%d"));
+            executor = Executors.newFixedThreadPool(numThreads, B2ExecutorUtils.createThreadFactory(APP_NAME + "-%d"));
         }
         return executor;
     }
@@ -107,8 +119,12 @@ public class B2 implements AutoCloseable {
                 b2.delete_bucket(remainingArgs);
             } else if ("list_buckets".equals(command)) {
                 b2.list_buckets(remainingArgs);
+            } else if ("list_file_versions".equals(command)) {
+                b2.list_file_versions(remainingArgs);
             } else if ("update_bucket".equals(command)) {
                 b2.update_bucket(remainingArgs);
+            } else if ("upload_file".equals(command)) {
+                b2.upload_file(remainingArgs);
             } else if ("version".equals(command)) {
                 b2.version(remainingArgs);
             } else {
@@ -123,20 +139,63 @@ public class B2 implements AutoCloseable {
     //
     ////////////////////////////////////////////////////////////////////////
 
-    private void checkArgs(boolean isOk, String errMsg) {
+    private void checkArgs(boolean isOk,
+                           String errMsg) {
         if (!isOk) {
             usageAndExit(errMsg);
         }
     }
 
-    private void checkArgCount(String[] args, int minCount, int maxCount) {
+    private void checkArgCount(String[] args,
+                               int minCount,
+                               int maxCount) {
         checkArgs(args.length >= minCount, "too few arguments");
         checkArgs(args.length <= maxCount, "too many arguments");
     }
-    private void checkArgCount(String[] args, int exactCount) {
+
+    private void checkArgCount(String[] args,
+                               int exactCount) {
         checkArgCount(args, exactCount, exactCount);
     }
 
+    private void checkArgCountIsAtLeast(String[] args,
+                                        int minCount) {
+        checkArgCount(args, minCount, Integer.MAX_VALUE);
+    }
+
+    private String getArgOrDie(String[] args,
+                               String arg,
+                               int iArg,
+                               int iLastArg) {
+        if (iArg > iLastArg) {
+            usageAndExit("missing argument for '" + arg + "'");
+        }
+        return args[iArg];
+    }
+
+    private int getIntArgOrDie(String[] args,
+                               String arg,
+                               int iArg,
+                               int iLastArg) {
+        final String asString = getArgOrDie(args, arg, iArg, iLastArg);
+        try {
+            return Integer.parseInt(asString);
+        } catch (NumberFormatException e) {
+            usageAndExit("argument for '" + arg  + "' must be an integer");
+            return 666;  // we never get here.
+        }
+    }
+    private int getPositiveIntArgOrDie(String[] args,
+                                       String arg,
+                                       int iArg,
+                                       int iLastArg) {
+        final String asString = getArgOrDie(args, arg, iArg, iLastArg);
+        int value = getIntArgOrDie(args, arg, iArg, iLastArg);
+        if (value <= 0) {
+            usageAndExit("argument for '" + arg + "' must be a POSITIVE integer");
+        }
+        return value;
+    }
 
     ////////////////////////////////////////////////////////////////////////
     //
@@ -153,6 +212,71 @@ public class B2 implements AutoCloseable {
 
         usageAndExit("can't find bucket named '" + bucketName + "'");
         throw new RuntimeException("usageAndExit never returns!");
+    }
+
+    private B2UploadFileRequest makeUploadRequestFromArgs(String[] args) throws B2Exception {
+        // [--sha1 <sha1sum>]
+        // [--contentType <contentType>]
+        // [--info <key>=<value>]*
+        // [--noProgress]
+        // [--threads N]
+        // <bucketName> <localFilePath> <b2FileName>
+
+        checkArgCountIsAtLeast(args, 3);
+        int iLastArg = args.length - 1;
+
+        final String b2Path = args[iLastArg];
+        iLastArg--;
+
+        final String localPath = args[iLastArg];
+        iLastArg--;
+
+        final String bucketName = args[iLastArg];
+        iLastArg--;
+
+        String sha1 = null;
+        String contentType = B2ContentTypes.B2_AUTO;
+        final Map<String,String> infos = new TreeMap<>();
+
+        for (int iArg=0; iArg <= iLastArg; iArg++) {
+            final String arg = args[iArg];
+            if ("--sha1".equals(arg)) {
+                iArg++;
+                sha1 = getArgOrDie(args, arg, iArg, iLastArg);
+            } else if ("--noProgress".equals(arg)) {
+                showProgress = false;
+            } else if ("--contentType".equals(arg)) {
+                iArg++;
+                contentType = getArgOrDie(args, arg, iArg, iLastArg);
+            } else if ("--threads".equals(arg)) {
+                iArg++;
+                numThreads = getPositiveIntArgOrDie(args, arg, iArg, iLastArg);
+            } else if ("--info".equals(arg)) {
+                iArg++;
+                final String pair = getArgOrDie(args, arg, iArg, iLastArg);
+                final String[] vParts = pair.split("=");
+                if (vParts.length != 2 || vParts[0].isEmpty()) {
+                    usageAndExit("bad format for argument to '" + arg + "' (" + pair + ")");
+                }
+                infos.put(vParts[0], vParts[1]);
+            } else {
+                usageAndExit("unexpected argument '" + arg + "'");
+            }
+        }
+
+        final B2Bucket bucket = getBucketByNameOrDie(bucketName);
+        final B2ContentSource source = B2FileContentSource
+                .builder(new File(localPath))
+                .setSha1(sha1)
+                .build();
+
+        return B2UploadFileRequest
+                .builder(bucket.getBucketId(),
+                        b2Path,
+                        contentType,
+                        source)
+                .setCustomFields(infos)
+                .build();
     }
 
     ////////////////////////////////////////////////////////////////////////
@@ -188,6 +312,27 @@ public class B2 implements AutoCloseable {
         for (B2Bucket bucket : client.buckets()) {
             out.println(bucket);
         }
+    }
+
+    private void list_file_versions(String[] args) throws B2Exception {
+        checkArgCount(args, 1, 4);
+        final B2Bucket bucket = getBucketByNameOrDie(args[0]);
+        for (B2FileVersion version : client.fileVersions(bucket.getBucketId())) {
+            out.println(version);
+        }
+    }
+
+    private void upload_file(String[] args) throws B2Exception {
+        B2UploadFileRequest request = makeUploadRequestFromArgs(args);
+        if (shouldBeSmallFile(request)) {
+            client.uploadSmallFile(request);
+        } else {
+            client.uploadLargeFile(request, getExecutor());
+        }
+    }
+
+    private boolean shouldBeSmallFile(B2UploadFileRequest request) {
+        return true;
     }
 
     private void version(String[] args) {
