f06c16639fd144fbd77cbf67a9bcd37406cb7954
ab <5149322+certainmagic@users.noreply.github.com>
2017-08-24 14:49:03 -0700

    uploading/testing: adding tests for B2UploadProgress.

---
+++ core/src/test/java/com/backblaze/b2/client/structures/B2UploadProgressTest.java
@@ -0,0 +1,35 @@
+package com.backblaze.b2.client.structures;/*
+ * Copyright 2017, Backblaze, Inc. All rights reserved. 
+ */
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class B2UploadProgressTest {
+    private final static int PART_INDEX = 1;
+    private final static int PART_COUNT = 2;
+    private final static long START_BYTE = 3;
+    private final static long LENGTH = 4;
+    private final static long BYTES_SO_FAR = 5;
+    private final static B2UploadState STATUS = B2UploadState.SUCCEEDED;
+
+    @Test
+    public void test() {
+        final B2UploadProgress progress = new B2UploadProgress(PART_INDEX,
+                PART_COUNT,
+                START_BYTE,
+                LENGTH,
+                BYTES_SO_FAR,
+                STATUS);
+
+        assertEquals(PART_INDEX, progress.getPartIndex());
+        assertEquals(PART_COUNT, progress.getPartCount());
+        assertEquals(START_BYTE, progress.getStartByte());
+        assertEquals(LENGTH, progress.getLength());
+        assertEquals(BYTES_SO_FAR, progress.getBytesSoFar());
+        assertEquals(STATUS, progress.getStatus());
+
+        assertEquals("B2UploadProgress{partIndex=1, partCount=2, startByte=3, length=4, bytesSoFar=5, status=SUCCEEDED}", progress.toString());
+    }
+}
