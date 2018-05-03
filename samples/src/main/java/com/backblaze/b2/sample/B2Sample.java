9a6aa1ecbf90e82b998707d957034d14c3c6cbe3
Valentin Popov <popov@mailarchiva.ru>
2018-05-03 20:29:05 +0300

    https://github.com/Backblaze/b2-sdk-java/issues/28

--- samples/src/main/java/com/backblaze/b2/sample/B2Sample.java
+++ samples/src/main/java/com/backblaze/b2/sample/B2Sample.java
@@ -251,12 +251,7 @@ public class B2Sample {
         B2FileVersion file2ByName = client.getFileInfoByName(bucket.getBucketName(), file2.getFileName());
         writer.println("file2ByName: " + file2ByName);
 
-        if (!file2again.getFileId().equals(file2ByName.getFileId()) ||
-            file2again.getContentLength() != file2ByName.getContentLength() ||
-            !file2again.getContentType().equals(file2ByName.getContentType()) ||
-            !file2again.getContentSha1().equals(file2ByName.getContentSha1()) ||
-            file2again.getUploadTimestamp() != file2ByName.getUploadTimestamp() ||
-            !file2again.getFileName().equals(file2ByName.getFileName())) {
+        if (!file2again.equals(file2ByName)) {
             throw new RuntimeException("file!");
         }
 
