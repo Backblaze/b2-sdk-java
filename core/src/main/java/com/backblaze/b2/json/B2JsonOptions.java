979d2d896cd5493bc37f993865ad02eab6f52205
Brian Beach <coder@beachfamily.net>
2018-09-06 15:02:43 -0400

    Add B2JsonOptions object and builder.
    This will replace the int bitmask used to pass options to B2Json.

---
+++ core/src/main/java/com/backblaze/b2/json/B2JsonOptions.java
@@ -0,0 +1,85 @@
+/*
+ * Copyright 2018, Backblaze Inc. All Rights Reserved.
+ * License https://www.backblaze.com/using_b2_code.html
+ */
+
+package com.backblaze.b2.json;
+
+/**
+ * Options used by JSON serialization and de-serialization.
+ */
+public class B2JsonOptions {
+
+    /**
+     * What to do with extra fields found when de-serializing.
+     */
+    enum ExtraFieldOption {
+        ERROR,   // Throw an exception if an unexpected field is found.
+        IGNORE   // Silently ignore extra fields.
+    }
+
+    /**
+     * What to do with extra fields found when de-serializing.
+     */
+    private final ExtraFieldOption extraFieldOption;
+
+    /**
+     * What version of the (de-)serialized structure to use.
+     *
+     * This affects which fields are included in the structure.  Only fields that are
+     * part of the given version are included.
+     */
+    private final int version;
+
+    /**
+     * Initialize a new B2JsonOptions.
+     */
+    private B2JsonOptions(ExtraFieldOption extraFieldOption, int version) {
+        this.extraFieldOption = extraFieldOption;
+        this.version = version;
+    }
+
+    /**
+     * What to do with extra fields found when de-serializing.
+     */
+    public ExtraFieldOption getExtraFieldOption() {
+        return extraFieldOption;
+    }
+
+    /**
+     * What version of the (de-)serialized structure to use.
+     */
+    public int getVersion() {
+        return version;
+    }
+
+    /**
+     * Returns a new builder for B2JsonOptions.
+     */
+    public static Builder builder() {
+        return new Builder();
+    }
+
+    /**
+     * Builder for B2JsonOptions
+     */
+    public static class Builder {
+
+        private ExtraFieldOption extraFieldOption = ExtraFieldOption.ERROR;
+        private int version = 1;
+
+        public Builder setExtraFieldOption(ExtraFieldOption extraFieldOption) {
+            this.extraFieldOption = extraFieldOption;
+            return this;
+        }
+
+        public Builder setVersion(int version) {
+            this.version = version;
+            return this;
+        }
+
+        public B2JsonOptions build() {
+            return new B2JsonOptions(extraFieldOption, version);
+        }
+    }
+}
