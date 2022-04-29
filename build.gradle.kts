26ea7ff38e864c310205e07bee0e61522bad60ce
Ola Nordstrom <ola.nordstrom@gmail.com>
2022-04-29 14:07:34 -0700

    add dependabot configuration (#17)

---
+++ build.gradle.kts
@@ -0,0 +1,4 @@
+// This file is empty as we don't need any project configuration at this level.
+// However, some tools don't work unless there is a build.gradle.kts file present,
+// as they don't know to look for settings.gradle.kts too. So this file is here just
+// to satisfy those tools.
